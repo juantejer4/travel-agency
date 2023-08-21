4cd00f3eea0c2a64507ca8c35aebca17203bb7c1
Juan Tejera <juan.tejera@lightit.io>
2023-08-21 10:31:06 -0300

    fead: add airlines layout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AirlineController;
 use App\Http\Controllers\CityController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/cities', [CityController::class, 'index']);
+
+Route::get('/airlines', [AirlineController::class, 'index']);
