77059b976cbf91591de27de86344137f89a5f097
Juan Tejera <juan.tejera@lightit.io>
2023-09-11 15:13:20 -0300

    test: flights post with valid params

--- tests/Feature/FlightControllerTest.php
+++ tests/Feature/FlightControllerTest.php
@@ -38,4 +38,25 @@ class FlightControllerTest extends TestCase
                 'arrival_time' => '2023-05-05T14:31:00',
             ]);
     }
+
+    /** @test */
+    public function testCreateAFlightWithValidParameters()
+    {
+        City::factory(2)->create();
+
+        Airline::factory()->create();
+
+        $flightData = [
+            'airline_id' => 1,
+            'origin_city_id' => 1,
+            'destination_city_id' => 2,
+            'departure_time' => '2023-09-11 11:18:27',
+            'arrival_time' => '2023-09-11 15:19:27'
+        ];
+
+        $this->json('POST', action([FlightController::class, 'store']), $flightData)
+            ->assertStatus(200)
+            ->assertJson($flightData);
+    }
+
 }
